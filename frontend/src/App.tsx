4933c86fd1c2950971b308541cf95cd72cdafef7
AmjustRalph <Raphaeltetteh2003@gmail.com>
2025-03-21 11:48:18 +0000

    Initial commit - Trello Clone progress

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./components/Login";
+import SignUp from "./components/sign-up"; // Ensure this is correctly imported
+import Navbar from "./components/Navbar";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} /> {/* Ensure this route exists */}
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
+
+
+
+
